034dc8f59d5cb9c29f6119f1edceb7d02d3b722d
Olga Assanowicz <o.assanowicz@gmail.com>
2015-02-04 10:36:48 +0100

    fixed email blank error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_filter :authenticate_user!, :except => [:show, :index, :info]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+end
 end
